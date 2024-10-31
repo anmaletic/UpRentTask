1398c3f0a0f3569a4cd7b915b772c6ec4c77501d
anmal <junkie_@msn.com>
2024-10-31 06:54:09 +0100

    Setup DI

--- UpRentTask/App.xaml.cs
+++ UpRentTask/App.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace UpRentTask
 {
@@ -9,6 +13,29 @@ namespace UpRentTask
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            ConfigureServices();
+        }
+        
+        private void ConfigureServices()
+        {
+            var config = AddConfiguration();
+            
+            Ioc.Default.ConfigureServices(new ServiceCollection()
+                .AddSingleton(config)
+                .BuildServiceProvider());
+            
+        }
+        
+        private IConfiguration AddConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+        
+            return builder.Build();
+        }
     }
 
 }
