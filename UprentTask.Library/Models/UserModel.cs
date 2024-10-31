f2b503c85386811ee23ff4421b4143542a3cd82a
anmal <junkie_@msn.com>
2024-10-31 08:46:08 +0100

    Create user model

---
+++ UprentTask.Library/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace UprentTask.Library.Models;
+
+public class UserModel
+{
+    public int UserId { get; set; }
+    public string Username { get; set; } = "";
+    public DateTime CreatedDate { get; set; }
+    public UserModel CreatedBy { get; set; } = null!;
+    public DateTime? ModifiedDate { get; set; }
+    public UserModel? ModifiedBy { get; set; }
+}
