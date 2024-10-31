70765755c78b54dc04baf0bbf902d983c77b3a9e
anmal <junkie_@msn.com>
2024-10-31 07:01:56 +0100

    Add global usings

---
+++ UpRentTask/GlobalUsings.cs
@@ -0,0 +1,8 @@
+// Global using directives
+
+global using System.IO;
+global using System.Windows;
+global using CommunityToolkit.Mvvm.DependencyInjection;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using UpRentTask.ViewModels;
