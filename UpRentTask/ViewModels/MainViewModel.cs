c141fba90fbe3ac050498dc626966812f1fa56b9
anmal <junkie_@msn.com>
2024-10-31 12:56:31 +0100

    Auto login user on app start

--- UpRentTask/ViewModels/MainViewModel.cs
+++ UpRentTask/ViewModels/MainViewModel.cs
@@ -2,16 +2,41 @@
 
 public partial class MainViewModel : ObservableObject
 {
-    [ObservableProperty] private UserControl _activeView = new UsersView();
+    private readonly ILoggedInUser _loggedInUser;
+    private readonly IUserService _userService;
+    [ObservableProperty] private UserControl _activeView;
 
-    public MainViewModel()
+    public MainViewModel(ILoggedInUser loggedInUser, IUserService userService)
     {
-        WeakReferenceMessenger.Default.Register<ChangeViewMessage>(this, (r, m) => 
+        _loggedInUser = loggedInUser;
+        _userService = userService;
+
+        WeakReferenceMessenger.Default.Register<ChangeViewMessage>(this, (r, m) =>
             ActiveView = m.View switch
+            {
+                "EditUsers" => new EditUsersView(),
+                "Users" => new UsersView(),
+                _ => throw new ArgumentOutOfRangeException()
+            });
+
+        Task.Run(async () => await Init());
+
+    }
+
+    private async Task Init()
+    {
+        var result = await _userService.GetById(1);
+
+        if (result is not null)
         {
-            "EditUsers" => new EditUsersView(),
-            "Users" => new UsersView(),
-            _ => throw new ArgumentOutOfRangeException()
-        });
+            _loggedInUser.UserId = result.UserId;
+            _loggedInUser.Username = result.Username;
+            _loggedInUser.CreatedBy = result.CreatedBy;
+            _loggedInUser.CreatedDate = result.CreatedDate;
+            _loggedInUser.ModifiedBy = result.ModifiedBy;
+            _loggedInUser.ModifiedDate = result.ModifiedDate;
+        }
+        
+        Application.Current.Dispatcher.Invoke(() => ActiveView = new UsersView());
     }
 }
