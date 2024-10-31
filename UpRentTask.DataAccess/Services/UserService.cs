c1f3460143c00dee84dc3ebd1566875daa5940cc
anmal <junkie_@msn.com>
2024-10-31 13:18:22 +0100

    Refactor Get methods

--- UpRentTask.DataAccess/Services/UserService.cs
+++ UpRentTask.DataAccess/Services/UserService.cs
@@ -16,67 +16,49 @@ public class UserService : IUserService
             .Include(u => u.ModifiedByUser)
             .FirstOrDefaultAsync(u => u.UserId == id);
 
-        if (result is null) return null;
-        
-        var user = new UserModel()
-        {
-            UserId = result.UserId,
-            Username = result.Username,
-            CreatedDate = result.CreatedDate,
-            ModifiedDate = result.ModifiedDate,
-            CreatedBy = new UserModel()
-            {
-                UserId = result.CreatedByUser.UserId,
-                Username = result.CreatedByUser.Username,
-                CreatedDate = result.CreatedByUser.CreatedDate,
-                ModifiedDate = result.CreatedByUser.ModifiedDate
-            },
-            ModifiedBy = result.ModifiedByUserId is null
-                ? null
-                : new UserModel()
-                {
-                    UserId = result.ModifiedByUser!.UserId,
-                    Username = result.ModifiedByUser.Username,
-                    CreatedDate = result.ModifiedByUser.CreatedDate,
-                    ModifiedDate = result.ModifiedByUser.ModifiedDate
-                }
-        };
-
-        return user;
+        return result is null ? null : MapToUserModel(result);
     }
-
+    
     public async Task<List<UserModel>> GetAll()
     {
         var result = await _context.Users
             .Include(u => u.CreatedByUser)
             .Include(u => u.ModifiedByUser)
+            .AsNoTracking()
             .ToListAsync();
 
-        var users = result.Select(u => new UserModel()
+        var users = result
+            .Select(MapToUserModel)
+            .ToList();
+
+        return users;
+    }
+
+    private UserModel MapToUserModel(User user)
+    {
+        return new UserModel()
         {
-            UserId = u.UserId,
-            Username = u.Username,
-            CreatedDate = u.CreatedDate,
-            ModifiedDate = u.ModifiedDate,
+            UserId = user.UserId,
+            Username = user.Username,
+            CreatedDate = user.CreatedDate,
+            ModifiedDate = user.ModifiedDate,
             CreatedBy = new UserModel()
             {
-                UserId = u.CreatedByUser.UserId,
-                Username = u.CreatedByUser.Username,
-                CreatedDate = u.CreatedByUser.CreatedDate,
-                ModifiedDate = u.CreatedByUser.ModifiedDate
+                UserId = user.CreatedByUser.UserId,
+                Username = user.CreatedByUser.Username,
+                CreatedDate = user.CreatedByUser.CreatedDate,
+                ModifiedDate = user.CreatedByUser.ModifiedDate
             },
-            ModifiedBy = u.ModifiedByUserId is null
+            ModifiedBy = user.ModifiedByUser is null
                 ? null
                 : new UserModel()
                 {
-                    UserId = u.ModifiedByUser!.UserId,
-                    Username = u.ModifiedByUser.Username,
-                    CreatedDate = u.ModifiedByUser.CreatedDate,
-                    ModifiedDate = u.ModifiedByUser.ModifiedDate
+                    UserId = user.ModifiedByUser.UserId,
+                    Username = user.ModifiedByUser.Username,
+                    CreatedDate = user.ModifiedByUser.CreatedDate,
+                    ModifiedDate = user.ModifiedByUser.ModifiedDate
                 }
-        }).ToList();
-
-        return users;
+        };
     }
     
     public async Task<bool> Delete(int deleteId, int modifyId )
