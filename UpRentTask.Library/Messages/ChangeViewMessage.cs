88741e08d6721f29e4aab7c8dcd3815568d19c3c
anmal <junkie_@msn.com>
2024-11-01 21:04:31 +0100

    Add parameter property

--- UpRentTask.Library/Messages/ChangeViewMessage.cs
+++ UpRentTask.Library/Messages/ChangeViewMessage.cs
@@ -1,6 +1,7 @@
 ﻿namespace UpRentTask.Library.Messages;
 
-public class ChangeViewMessage(string view)
+public class ChangeViewMessage(string view, Dictionary<string, string>? parameters = null)
 {
     public string View { get; set; } = view;
+    public Dictionary<string, string>? Parameters { get; set; } = parameters;
 }
