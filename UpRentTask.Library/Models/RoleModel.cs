588e82d7ef570e46c2b3a67a589ec559e42c3650
anmal <junkie_@msn.com>
2024-11-01 21:04:02 +0100

    Implement property changed notication

--- UpRentTask.Library/Models/RoleModel.cs
+++ UpRentTask.Library/Models/RoleModel.cs
@@ -1,8 +1,33 @@
-﻿namespace UpRentTask.Library.Models;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-public class RoleModel
+namespace UpRentTask.Library.Models;
+
+public class RoleModel : INotifyPropertyChanged
 {
+    private bool _isSelected;
     public int Id { get; set; }
     public string Name { get; set; }
-    public bool IsSelected { get; set; }
+
+    public bool IsSelected
+    {
+        get => _isSelected;
+        set
+        {
+            if (value == _isSelected)
+            {
+                return;
+            }
+
+            _isSelected = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+   
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
