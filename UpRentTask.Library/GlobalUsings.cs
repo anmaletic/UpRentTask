add1fd5f374023cab4c9b2ab0ac968d4df789f1b
anmal <junkie_@msn.com>
2024-11-03 13:28:19 +0100

    Add global usings

--- UpRentTask.Library/GlobalUsings.cs
+++ UpRentTask.Library/GlobalUsings.cs
@@ -4,5 +4,8 @@ global using System.ComponentModel;
 global using System.Runtime.CompilerServices;
 global using System.Windows;
 global using System.Windows.Controls;
+global using CommunityToolkit.Mvvm.DependencyInjection;
 global using FluentValidation;
+global using Microsoft.Extensions.DependencyInjection;
+global using UpRentTask.Library.Interfaces;
 global using UpRentTask.Library.Models;
